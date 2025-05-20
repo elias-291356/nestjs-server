2f11b280d0e22ca3b14b8e621a5ec14baf1b30cc
elias-291356 <ikucher.002@gmail.com>
2025-05-20 17:07:46 +0200

    re-CAPTCHA

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,23 +12,19 @@ import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { Request, Response } from 'express';
 import { LoginDto } from './dto/login.dto';
+import { Recaptcha } from '@nestlab/google-recaptcha';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  // @Recaptcha()
-
+  @Recaptcha()
   @Post('register')
   @HttpCode(HttpStatus.OK)
   public async register(@Req() req: Request, @Body() dto: RegisterDto) {
     return this.authService.register(req, dto);
   }
 
-  // public async register(@Body() dto: RegisterDto) {
-  //   return this.authService.register(dto);
-  // }
-
-  // @Recaptcha()
+  @Recaptcha()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   public async login(@Req() req: Request, @Body() dto: LoginDto) {
