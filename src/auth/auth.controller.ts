1f9730f46bfb413d953d798a9a69f5b66c81ce8b
elias-291356 <ikucher.002@gmail.com>
2025-05-21 11:08:34 +0200

    google auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,17 @@
 import {
+  BadRequestException,
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
+  NotFoundException,
+  Param,
   Post,
+  Query,
   Req,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
@@ -13,10 +19,17 @@ import { RegisterDto } from './dto/register.dto';
 import { Request, Response } from 'express';
 import { LoginDto } from './dto/login.dto';
 import { Recaptcha } from '@nestlab/google-recaptcha';
+import { AuthProviderGuard } from './guards/provider.guard';
+import { ConfigService } from '@nestjs/config';
+import { ProviderService } from './provider/provider.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+    private readonly providerService: ProviderService,
+  ) {}
   @Recaptcha()
   @Post('register')
   @HttpCode(HttpStatus.OK)
@@ -38,5 +51,39 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ) {
     return this.authService.logout(req, res);
+  }
+
+  // ===== OAuth
+
+  @UseGuards(AuthProviderGuard)
+  @Get('/oauth/connect/:provider')
+  public async connect(@Param('provider') provider: string) {
+    const providerInstance = this.providerService.findByService(provider);
+
+    if (!providerInstance) {
+      throw new NotFoundException(`Провайдер ${provider} не найден`);
+    }
+    return {
+      url: providerInstance.getAuthUrl(),
+    };
+  }
+
+  @UseGuards(AuthProviderGuard)
+  @Get('/oauth/callback/:provider')
+  public async callback(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+    @Query('code') code: string,
+    @Param('provider') provider: string,
+  ) {
+    if (!code) {
+      throw new BadRequestException('Не был предоставлен код авторизации.');
+    }
+
+    await this.authService.extractProfileFromCode(req, provider, code);
+
+    return res.redirect(
+      `${this.configService.getOrThrow<string>('ALLOWED_ORIGIN')}/dashboard/settings`,
+    );
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,12 +12,16 @@ import { Request, Response } from 'express';
 import { LoginDto } from './dto/login.dto';
 import { verify } from 'argon2';
 import { ConfigService } from '@nestjs/config';
+import { ProviderService } from './provider/provider.service';
+import { PrismaService } from '@/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   public constructor(
+    private readonly prismaService: PrismaService,
     private readonly userService: UserService,
     private readonly configService: ConfigService,
+    private readonly providerService: ProviderService,
   ) {}
 
   // public async register(dto: RegisterDto) {
@@ -87,6 +91,60 @@ export class AuthService {
     //   dto.code,
     // );
     // }
+
+    return this.saveSession(req, user);
+  }
+
+  public async extractProfileFromCode(
+    req: Request,
+    provider: string,
+    code: string,
+  ) {
+    const providerInstance = this.providerService.findByService(provider);
+
+    if (!providerInstance) {
+      throw new NotFoundException(`Провайдер ${provider} не найден`);
+    }
+
+    const profile = await providerInstance.findUserByCode(code);
+
+    const account = await this.prismaService.account.findFirst({
+      where: {
+        id: profile.id,
+        provider: profile.provider,
+      },
+    });
+
+    let user = account?.userId
+      ? await this.userService.findById(account.userId)
+      : null;
+
+    if (user) {
+      return this.saveSession(req, user);
+    }
+
+    user = await this.userService.create(
+      profile.email,
+      '',
+      profile.name,
+      profile.picture,
+      AuthMethod[profile.provider.toUpperCase()],
+      true,
+    );
+
+    if (!account) {
+      await this.prismaService.account.create({
+        data: {
+          userId: user.id,
+          type: 'oauth',
+          provider: profile.provider,
+          accessToken: profile.access_token,
+          refreshToken: profile.refresh_token,
+
+          expiresAt: profile.expires_at ?? Date.now() + 3600 * 1000,
+        },
+      });
+    }
 
     return this.saveSession(req, user);
   }
